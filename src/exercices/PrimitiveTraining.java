23fa121aefae90b70ed8ca6b241d997631bcbf9f
Arsene Ella <aellalongin@gmai.com>
2019-10-15 11:43:17 +0200

    edit PrimitiveTraining

--- src/exercices/PrimitiveTraining.java
+++ src/exercices/PrimitiveTraining.java
@@ -2,17 +2,31 @@ public class PrimitiveTraining {
 
     // TODO 1 : Change the method 'integer' to return the expected result : int 42
     public static int integer() {
-        return 0;
+        return 42;
     }
 
     // TODO 2 : Create a method 'str', result expected : String "Hello"
+    public static String str() {
+        return "Hello";
+    }
 
 
     // TODO 3 : Create a method 'real', result expected : double 3.14
+    public static double real() {
+    	double pi = 3.14;
+        return pi;
+    }
 
 
     // TODO 4 : Create a method 'bool', result expected : boolean false
+    public boolean bool() {
+        return false;
+    }
 
 
     // TODO 5 : Create a method 'nothing', no result expected
+    public void nothing() {
+        return;
+    }
+    
 }
